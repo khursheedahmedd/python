2251f9670b11ba12db36334882d52b23b97fcc43
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-08-27 16:52:54 +0500

    add basics

---
+++ basics/FirstProgram.py
@@ -0,0 +1,15 @@
+print('Hello, YOU')
+
+name = 'Khursheed'
+age = 21
+height = 5.10
+
+print('Name: ', name)
+print ('Age: ', age)
+print ('Height: ', height)
+
+print('\n')
+
+print(type(name))
+print(type(age))
+print(type(height))
