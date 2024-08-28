a4eae4395a6d863ec8cbc15b18223bdcb7f50e84
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-08-28 15:17:20 +0500

    add for loop

---
+++ loops/ForLoop.py
@@ -0,0 +1,27 @@
+nums = [4, 2, 5 ,2, "jdsdds"]
+
+for num in nums:
+    print(num)
+
+tup =(4, 2, 5 ,2, 4, "you")
+
+for el in tup: 
+    print(el)
+else: 
+    print("End")
+    
+for el in tup: 
+    if(el == 4):
+        print("You")
+        break
+else: 
+    ("me")
+    
+# #range(start?, stop, index)
+# seq = range(7)
+# for i in seq: 
+#     print(i)
+    
+newSeq = range(2, 12, 3)
+for i in newSeq: 
+    print(i)
