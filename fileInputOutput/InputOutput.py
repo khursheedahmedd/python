9fb2f01ef06fe17dddf28e49eb24e84099b1d0ff
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-08-28 21:28:18 +0500

    add file I/O

---
+++ fileInputOutput/InputOutput.py
@@ -0,0 +1,16 @@
+# f = open("demo.txt", "a")
+# f = open("demo.txt", "r")
+# f = open("demo.txt", "r+")
+# f = open("demo.txt", "w")
+f = open("demo.txt", "w+")
+
+# print(f.read())
+# print(f.read(12))
+# print(f.readline())
+f.write("\nI want to learn Python")
+# print(f.read())
+f.close()
+
+# import os
+
+# os.remove("sample.txt")
